cee7d39c97560be0ee869f947894a02c99d61d0c
C1281192 Felipe Barbosa Ferreira <c1281192@interno.bb.com.br>
2020-07-14 19:56:20 -0300

    start project

---
+++ src/test/java/github/com/felipebarbosaferreira/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package github.com.felipebarbosaferreira;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
